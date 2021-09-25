4f9f2387a9925ce7372c8af94af077f124693289
devlulcas <lucasalvesrego.contato@gmail.com>
2021-09-25 20:05:38 -0300

    add: example class kotlin

---
+++ src/Classes/PessoaKotlin.kt
@@ -0,0 +1,13 @@
+package Classes
+
+// Exemplo de classe em Kotlin
+class PessoaKotlin(
+    val nome: String,
+    val idade: Int
+)
+
+/*
+* Essa classe aqui no caso é uma classe que contém apenas dados
+* Um objeto dessa classe é um value object
+* Public é a visibilidade padrão
+* */
